ea8ba307171fbe76f4ad82c7814283738f8b0881
Phantom-Xen <141147124+Phantom-Xen@users.noreply.github.com>
2023-10-30 15:15:40 +0100

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,13 @@
+# Simple Python Program Without Imports
+
+# Define a function to calculate the sum of two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate and display the sum
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}")
